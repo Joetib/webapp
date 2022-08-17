af5ad6e957780610b42326b8d76ff61d2555af77
Oti Boateng Joseph <otiboatengjoe@gmail.com>
2022-08-17 14:59:24 +0000

    Added homepagetestcase

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,8 @@
+from http import HTTPStatus
 from django.test import TestCase
 
 # Create your tests here.
+class HomePageTestCase(TestCase):
+    def test_homepage_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
