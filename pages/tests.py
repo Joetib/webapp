b9ac6bdc6f53ca1517b6a318c88cb963b96e90b2
Oti Boateng Joseph <otiboatengjoe@gmail.com>
2022-08-17 16:23:48 +0000

    Tests fixed

--- pages/tests.py
+++ pages/tests.py
@@ -5,4 +5,4 @@ from django.test import TestCase
 class HomePageTestCase(TestCase):
     def test_homepage_works(self):
         response = self.client.get("/")
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
