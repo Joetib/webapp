e40bf4e5276c344312765e5dbc8e8786eacf7acc
Joetib <otiboatengjoe@gmail.com>
2023-11-16 19:39:48 +0000

    auto-generated tests for pages on Thursday November 16 2023, 19:39:00

---
+++ pages/tests/test_views.py
@@ -0,0 +1,17 @@
+# test_views.py
+import pytest
+from django.urls import reverse
+from django.test import RequestFactory
+from pages import views
+
+@pytest.mark.django_db
+class TestViews:
+    @classmethod
+    def setup_class(cls):
+        cls.factory = RequestFactory()
+
+    def test_homepage_view(self):
+        request = self.factory.get(reverse('homepage'))
+        response = views.homepage(request)
+        assert response.status_code == 200
+        assert "homepage.html" in (t.name for t in response.templates)
